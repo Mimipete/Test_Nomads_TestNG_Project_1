3a4daccde800cc750c5e46471b00aaefa26c6e74
umiteser <eser_umit@hotmail.com>
2024-03-24 22:14:05 +0300

    Data provider düzenlemeleri yapıldı.

--- src/US_401/CheckingLoginErrorsInTheSystem.java
+++ src/US_401/CheckingLoginErrorsInTheSystem.java
@@ -3,21 +3,43 @@ package US_401;
 import POM.LoginContent;
 import Utility.BaseDriver;
 import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 public class CheckingLoginErrorsInTheSystem extends BaseDriver {
-    @Test
-    public void TC_CheckingLoginErrorsInTheSystem() {
+    @Test(dataProvider = "credentials", groups = {"Smoke", "Login"})
+    public void TC_CheckingLoginErrorsInTheSystem(String username, String password, int location) {
 
         LoginContent lc = new LoginContent();
         myJsClick(lc.demoButton);
         myClick(lc.exploreOpenMRS2);
         myJsClick(lc.exploreOpenMRS2Demo);
-        mySendKeys(lc.username, "");
-        mySendKeys(lc.password, "");
+        mySendKeys(lc.username, username);
+        mySendKeys(lc.password, password);
+        if (location!=8){
+            myClick(lc.locations.get(location));
+        }
         myClick(lc.logInButton);
-        Assert.assertEquals(lc.LocationAlert.getText(), "You must choose a location!", "Login basarili");
+        if (location==8){
+            Assert.assertEquals(lc.LocationAlert.getText(), "You must choose a location!", "Login basarili");
+        }
+        else ()
+
+
 
     }
 
+    @DataProvider
+    Object[][] credentials() {
+        Object[][] invalidDatas =
+                {
+                        {"admin", "", 1},
+                        {"", "Admin123",0},
+                        {"admin", "Admin123",8},
+                        {"", "", 4},
+                        {"admin", "", 8},
+                        {"", "", 8},
+                };
+        return invalidDatas;
+    }
 }
