5b56fddff89f52c99d6ff189136b798c5f395a16
Ayse Ihtiyar <ayseeihtiyar@gmail.com>
2024-03-26 20:21:39 +0300

    Edited 'HomePageConnect', added 'patientInformation' element.

--- src/POM/HomePageContent.java
+++ src/POM/HomePageContent.java
@@ -9,34 +9,46 @@ import java.util.List;
 
 public class HomePageContent {
     public HomePageContent() {
-        PageFactory.initElements(BaseDriver.driver,this);
+        PageFactory.initElements(BaseDriver.driver, this);
     }
+
     @FindBy(xpath = "//ul[@class='navbar-nav ml-auto user-options']/li[3]/a")
     public WebElement logoutButton;
+
     @FindBy(xpath = "//ul[@class='navbar-nav ml-auto user-options']/li[1]")
     public WebElement profileIcon;
 
     // Register a patient
     @FindBy(css = "div[id='apps'] :nth-child(4)")
     public WebElement registerAPatientButton;
+
     @FindBy(name = "givenName")
     public WebElement given;
+
     @FindBy(name = "familyName")
     public WebElement familyName;
+
     @FindBy(id = "next-button")
     public WebElement nextButton;
+
     @FindBy(css = "select[id='gender-field']")
     public WebElement gender;
+
     @FindBy(name = "birthdateDay")
     public WebElement day;
-    @FindBy(css ="select[name='birthdateMonth']")
+
+    @FindBy(css = "select[name='birthdateMonth']")
     public WebElement month;
+
     @FindBy(name = "birthdateYear")
     public WebElement year;
+
     @FindBy(id = "address1")
     public WebElement address;
+
     @FindBy(xpath = "//input[@id = 'submit']")
     public WebElement confirm;
+
 
     // Patient's page
     @FindBy(css = "span[class='PersonName-givenName']")
@@ -55,5 +67,7 @@ public class HomePageContent {
     @FindBy(id = "patient-search")
     public WebElement patientSearch;
 
+    @FindBy(css = "div[class='col-12 col-lg-6']>div")
+    public List<WebElement> patientInformation;
 
 }
