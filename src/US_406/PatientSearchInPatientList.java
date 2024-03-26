57664984f31187e52e0dc22a34ca3b4721429908
Ayse Ihtiyar <ayseeihtiyar@gmail.com>
2024-03-26 20:27:54 +0300

    The positive scenario of 'TC_PatientSearchInPatientList' is complete.

--- src/US_406/PatientSearchInPatientList.java
+++ src/US_406/PatientSearchInPatientList.java
@@ -4,35 +4,38 @@ import POM.HomePageContent;
 import POM.LoginContent;
 import Utility.BaseDriver;
 import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.time.Duration;
+
 public class PatientSearchInPatientList extends BaseDriver {
-    @Test
+    @Test(groups = {"PatientManagement"})
     public void TC_PatientSearchInPatientList(){
+        WebDriverWait wait = new WebDriverWait(BaseDriver.driver, Duration.ofSeconds(20));
         LoginContent lc = new LoginContent();
         HomePageContent hpc = new HomePageContent();
-        //-----
+
         myJsClick(lc.demoButton);
         myClick(lc.exploreOpenMRS2);
         myJsClick(lc.exploreOpenMRS2Demo);
-        //------
+
         mySendKeys(lc.username, "admin");
         mySendKeys(lc.password, "Admin123");
         myClick(lc.locations.get(RandomGenerator(lc.locations.size()-1, 1)));
         myClick(lc.logInButton);
 
         myClick(hpc.findPatientRecord);
-        System.out.println("**1. "+hpc.patientRecordList.getFirst().getText());
-        mySendKeys(hpc.patientSearch,"100JNX");
-        System.out.println("** "+hpc.patientRecordList.getFirst().getText());
+        mySendKeys(hpc.patientSearch,"100J7W");
+        wait.until(ExpectedConditions.textToBe(By.xpath("//tr[@class='odd']/td[2]"),"deneme1 deneme2"));
+        Assert.assertTrue(hpc.patientRecordList.getFirst().isEnabled());
+        myClick(hpc.patientRecordList.getFirst());
 
-
-        //System.out.println(driver.findElement(By.cssSelector("1. "+"tr[class='odd']>td[class='dataTables_empty']")).getText());
-        //System.out.println(driver.findElement(By.cssSelector("2. "+"td[class='dataTables_empty']")).getText());
-        System.out.println(hpc.patientRecordList.get(0).findElement(By.cssSelector("[class='odd']>td")).getText().substring(0,6));
-        Assert.assertTrue(hpc.patientRecordList.get(0).findElement(By.cssSelector("[class='odd']>td")).getText().contains("100JNX"));
+        for (int i = 0; i < hpc.patientInformation.size(); i++) {
+            Assert.assertTrue(hpc.patientInformation.get(i).isDisplayed());
+        }
 
     }
 }
