90d43576813872774106a5c33366d0d8c081016c
Ayse Ihtiyar <ayseeihtiyar@gmail.com>
2024-03-26 23:47:14 +0300

    US_406

--- src/US_406/PatientSearchInPatientList.java
+++ src/US_406/PatientSearchInPatientList.java
@@ -4,6 +4,7 @@ import POM.HomePageContent;
 import POM.LoginContent;
 import Utility.BaseDriver;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
@@ -13,7 +14,7 @@ import java.time.Duration;
 
 public class PatientSearchInPatientList extends BaseDriver {
     @Test(groups = {"PatientManagement"})
-    public void TC_PatientSearchInPatientList(){
+    public void TC_PatientSearchInPatientList() {
         WebDriverWait wait = new WebDriverWait(BaseDriver.driver, Duration.ofSeconds(20));
         LoginContent lc = new LoginContent();
         HomePageContent hpc = new HomePageContent();
@@ -24,18 +25,28 @@ public class PatientSearchInPatientList extends BaseDriver {
 
         mySendKeys(lc.username, "admin");
         mySendKeys(lc.password, "Admin123");
-        myClick(lc.locations.get(RandomGenerator(lc.locations.size()-1, 1)));
+        myClick(lc.locations.get(RandomGenerator(lc.locations.size() - 1, 0)));
         myClick(lc.logInButton);
 
         myClick(hpc.findPatientRecord);
-        mySendKeys(hpc.patientSearch,"100JDJ");
-        wait.until(ExpectedConditions.textToBe(By.xpath("//tr[@class='odd']/td[2]"),"Test Nomads"));
+        mySendKeys(hpc.patientSearch, "100K8T");
+        wait.until(ExpectedConditions.textToBe(By.xpath("//tr[@class='odd']/td[2]"), "Test Nomads"));
         Assert.assertTrue(hpc.patientRecordList.getFirst().isEnabled());
-        myClick(hpc.patientRecordList.getFirst());
+        myJsClick(hpc.patientRecordList.getFirst());
 
         for (int i = 0; i < hpc.patientInformation.size(); i++) {
             Assert.assertTrue(hpc.patientInformation.get(i).isDisplayed());
         }
+
+        //  WebElement button=driver.findElement(By.cssSelector("[class='icon-home small']"));
+        //  myJsClick(button);
+        //  myClick(hpc.findPatientRecord);
+            ///**************
+        driver.navigate().back();
+        mySendKeys(hpc.patientSearch, "100ABC");
+        WebElement hata = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("[class='dataTables_empty']")));
+        System.out.println(hata.getText());
+        Assert.assertEquals(hata.getText(), "No matching records found", "Hatalı");
 
     }
 }
