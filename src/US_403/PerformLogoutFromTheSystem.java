97a5e9d830da40401d3a9aeefe82ab6633777d89
Ayse Ihtiyar <ayseeihtiyar@gmail.com>
2024-03-27 03:50:07 +0300

    Fixed 'Assert.assertEquals' error message. Added 'Smoke' and 'Login' groups. Removed extra line spaces.

--- src/US_403/PerformLogoutFromTheSystem.java
+++ src/US_403/PerformLogoutFromTheSystem.java
@@ -7,38 +7,20 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class PerformLogoutFromTheSystem extends BaseDriver {
-    @Test
-    public void TC_PerformLogoutFromTheSystem(){
-
-        LoginContent lc=new LoginContent();
-
-        HomePageContent hpc=new HomePageContent();
-
+    @Test(groups = {"Smoke", "Logout"})
+    public void TC_PerformLogoutFromTheSystem() {
+        LoginContent lc = new LoginContent();
+        HomePageContent hpc = new HomePageContent();
         myJsClick(lc.demoButton);
-
         myClick(lc.exploreOpenMRS2);
-
         myJsClick(lc.exploreOpenMRS2Demo);
-
         mySendKeys(lc.username, "admin");
-
         mySendKeys(lc.password, "Admin123");
-
         myClick(lc.locations.getFirst());
-
         myClick(lc.logInButton);
-
-        Assert.assertEquals(hpc.selectedLocation.getText(), "Inpatient Ward" );
-
+        Assert.assertEquals(hpc.selectedLocation.getText(), "Inpatient Ward");
         Assert.assertTrue(hpc.logoutButton.isDisplayed());
-
         myClick(hpc.logoutButton);
-
-        Assert.assertEquals(driver.getTitle(),"Login","Login başarısız!");
-
-
-
-
-
+        Assert.assertEquals(driver.getTitle(), "Login", "Failed to return to the login page.");
     }
 }
